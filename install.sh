5cfda6955ecea5dc82e3bf9fe44235c19b607018
Rob Zwissler <rob@zwissler.org>
2016-01-24 18:37:49 -0800

    Updated install script

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ if [ -f /etc/init.d/bearDropper ] ; then
   /etc/init.d/bearDropper stop
 fi
 echo -e '\nRetrieving and installing latest version...'
-wget -O /etc/init.d/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/src/init.d/bearDropper 
-wget -O /etc/config/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/src/config/bearDropper
-wget -O /usr/sbin/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/bearDropper
+wget -qO /etc/init.d/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/src/init.d/bearDropper 
+wget -qO /etc/config/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/src/config/bearDropper
+wget -qO /usr/sbin/bearDropper http://cdn.rawgit.com/robzr/bearDropper/master/bearDropper
 chmod 755 /usr/sbin/bearDropper /etc/init.d/bearDropper
 echo -e '\nRunning on historical log data'
 /usr/sbin/bearDropper -m entire -f stdout
