3f162ff83de5fc8a2967ea3c42dbd31b2a2d3fa6
Rob Zwissler <rob@zwissler.org>
2016-03-15 14:59:26 -0700

    Updated install script, updated bearDropper for speed improvement
    bearDropper was doing an iptables -L in a spot where it was run many
    times, since the -L does PTR resolution, depending on the hosts which
    are being looked up this can be very slow.  Added a -n.

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ if [ -f /etc/init.d/bearDropper ] ; then
   echo Detected previous version of bearDropper - stopping
   /etc/init.d/bearDropper stop
 fi
-echo -e 'Retrieving and installing latest version...'
+echo -e 'Retrieving and installing latest version'
 wget -qO /etc/init.d/bearDropper http://rawgit.com/robzr/bearDropper/master/src/init.d/bearDropper 
 wget -qO /etc/config/bearDropper http://rawgit.com/robzr/bearDropper/master/src/config/bearDropper
 wget -qO /usr/sbin/bearDropper http://rawgit.com/robzr/bearDropper/master/bearDropper
 chmod 755 /usr/sbin/bearDropper /etc/init.d/bearDropper
-echo -e 'Processing historical log data'
+echo -e 'Processing historical log data (this can take a while)'
 /usr/sbin/bearDropper -m entire -f stdout
 echo -e 'Starting background process'
 /etc/init.d/bearDropper enable
